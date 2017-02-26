6fbd5aefdec15e3b86c43082b7a4f4063bae6798
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-02-26 18:07:19 +0300

    Assignment 3: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -4,7 +4,7 @@
             .html5Mode({
                 enabled: true,
                 requireBase: false
-        });
+            });
         
         $stateProvider
             .state('landing', {
@@ -14,7 +14,11 @@
             .state('album',{
                 url: '/album',
                 templateUrl: '/templates/album.html'
-        });
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
     }
     angular
         .module('blocJams', ['ui-router'])
